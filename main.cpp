9477c7bd3f587004d0d0d918a2e0a85342e1c29f
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-11-12 11:12:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
+/*Qui vanno definite le variabili*/ int a=0, b=0, c=0, /*Area Triangolo*/ Atriang=0, /*Area Quadrato*/ Aq=0, /*Area Rettangolo*/ Ar=0, /*Area Trapezio*/ Atrap=0;
 cin >> a >> b >> c;
-
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+Atriang=(a*b)/2;
+Aq=a*a;
+Ar=a*b;
+Atrap=((a+b)*c)/2;
+cout << Atriang << endl << Aq << endl << Ar << endl << Atrap;
+return 0;
 }
